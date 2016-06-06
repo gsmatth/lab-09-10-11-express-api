ea625e60acc2e2edc0ac78b229f47839795e6310
Doug Popadince <gsmatth@icloud.com>
2016-06-06 16:03:02 -0700

    added error-response module and retested.  Lab 10 mvp is complete.

--- lab-doug/server.js
+++ lab-doug/server.js
@@ -1,12 +1,16 @@
 'use strict';
 const debug = require('debug')('matchscore:server');
 const express = require('express');
+const errorResponse = require('./lib/error-response');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const matchscoreRouter = require('./route/matchscore-router');
 const port = process.env.PORT || 3000;
 /*The app object denotes the Express application.  The app returned by express() is in fact a JavaScript Function, designed to be passed to Node’s HTTP servers as a callback to handle requests.*/
 const app = express();
 app.use(bodyParser.json());
+app.use(errorResponse);
+app.use(morgan('dev'));
 /*loads the router module in the app, matchscoreRouter is an instance of Router object.  In express, the term "use" mounts the specified middleware function or functions at the specified path*/
 app.use('/api/matchscore', matchscoreRouter);
 //default for routes that fall through route above.  The 'all' means all methods(GET, POST....)'
